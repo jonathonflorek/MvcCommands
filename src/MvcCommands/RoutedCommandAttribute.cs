b593c90f44990982d7833d07af89fd976536309a
jflorek <32914156+jflorek@users.noreply.github.com>
2019-01-14 20:32:02 -0600

    added routed command attribute

---
+++ src/MvcCommands/RoutedCommandAttribute.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+
+namespace MvcCommands
+{
+    [AttributeUsage(System.AttributeTargets.Class, Inherited = false, AllowMultiple = true)]
+    public sealed class RoutedCommandAttribute : Attribute
+    {
+        public RoutedCommandAttribute(String route, params String[] methods)
+        {
+            Route = route ?? throw new ArgumentNullException(nameof(route));
+            Methods = methods ?? throw new ArgumentNullException(nameof(methods));
+        }
+
+        public String Route { get; }
+
+        public IEnumerable<String> Methods { get; }
+    }
+}
